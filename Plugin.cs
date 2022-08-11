6c169564cbb5c49558c58856176c0287c8a65b06
AzumattDev <azumattgaming@gmail.com>
2022-08-11 14:55:39 -0400

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,141 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using ItemManager;
+using ServerSync;
+using UnityEngine;
+
+namespace DeedsOfUtgard
+{
+    [BepInPlugin(ModGUID, ModName, ModVersion)]
+    public class DeedsOfUtgardPlugin : BaseUnityPlugin
+    {
+        internal const string ModName = "DeedsOfUtgard";
+        internal const string ModVersion = "1.0.0";
+        internal const string Author = "Azumatt";
+        private const string ModGUID = Author + "." + ModName;
+        private static string ConfigFileName = ModGUID + ".cfg";
+        private static string ConfigFileFullPath = Paths.ConfigPath + Path.DirectorySeparatorChar + ConfigFileName;
+
+        internal static string ConnectionError = "";
+
+        private readonly Harmony _harmony = new(ModGUID);
+
+        public static readonly ManualLogSource DeedsOfUtgardLogger =
+            BepInEx.Logging.Logger.CreateLogSource(ModName);
+
+        private static readonly ConfigSync ConfigSync = new(ModGUID)
+            { DisplayName = ModName, CurrentVersion = ModVersion, MinimumRequiredVersion = ModVersion };
+
+        public void Awake()
+        {
+            ConfigSync.IsLocked = true;
+
+            /*
+             * Piece name naming convention
+             *
+             * $item_deed_utgard_10m
+             *
+             * $item_deed_10m
+             *
+             * */
+            
+            /* Normal Deeds */
+            Item deed10M = new("deedsofutgard", "Deed10m");
+            Item deed20M = new("deedsofutgard", "Deed20m");
+            Item deed50M = new("deedsofutgard", "Deed50m");
+            deed10M.Name.English("Deed 10m");
+            deed20M.Name.English("Deed 20m");
+            deed50M.Name.English("Deed 50m");
+            deed10M.Description.English("Deed 10m");
+            deed20M.Description.English("Deed 20m");
+            deed50M.Description.English("Deed 50m");
+
+            /* Utgard Deeds */
+            Item deedutgard10M = new("deedsofutgard", "DeedUtgard10m");
+            Item deedutgard20M = new("deedsofutgard", "DeedUtgard20m");
+            Item deedutgard50M = new("deedsofutgard", "DeedUtgard50m");
+            deedutgard10M.Name.English("Deed Utgard 10m");
+            deedutgard20M.Name.English("Deed Utgard 20m");
+            deedutgard50M.Name.English("Deed Utgard 50m");
+            deedutgard10M.Description.English("Deed Utgard 10m");
+            deedutgard20M.Description.English("Deed Utgard 20m");
+            deedutgard50M.Description.English("Deed Utgard 50m");
+
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            _harmony.PatchAll(assembly);
+            SetupWatcher();
+        }
+
+        private void OnDestroy()
+        {
+            Config.Save();
+        }
+
+        private void SetupWatcher()
+        {
+            FileSystemWatcher watcher = new(Paths.ConfigPath, ConfigFileName);
+            watcher.Changed += ReadConfigValues;
+            watcher.Created += ReadConfigValues;
+            watcher.Renamed += ReadConfigValues;
+            watcher.IncludeSubdirectories = true;
+            watcher.SynchronizingObject = ThreadingHelper.SynchronizingObject;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void ReadConfigValues(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(ConfigFileFullPath)) return;
+            try
+            {
+                DeedsOfUtgardLogger.LogDebug("ReadConfigValues called");
+                Config.Reload();
+            }
+            catch
+            {
+                DeedsOfUtgardLogger.LogError($"There was an issue loading your {ConfigFileName}");
+                DeedsOfUtgardLogger.LogError("Please check your config entries for spelling and format!");
+            }
+        }
+
+
+        #region ConfigOptions
+
+        private static ConfigEntry<bool> _serverConfigLocked = null!;
+        private static ConfigEntry<bool>? _recipeIsActiveConfig = null!;
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, ConfigDescription description,
+            bool synchronizedSetting = true)
+        {
+            ConfigDescription extendedDescription =
+                new(
+                    description.Description +
+                    (synchronizedSetting ? " [Synced with Server]" : " [Not Synced with Server]"),
+                    description.AcceptableValues, description.Tags);
+            ConfigEntry<T> configEntry = Config.Bind(group, name, value, extendedDescription);
+            //var configEntry = Config.Bind(group, name, value, description);
+
+            SyncedConfigEntry<T> syncedConfigEntry = ConfigSync.AddConfigEntry(configEntry);
+            syncedConfigEntry.SynchronizedConfig = synchronizedSetting;
+
+            return configEntry;
+        }
+
+        private ConfigEntry<T> config<T>(string group, string name, T value, string description,
+            bool synchronizedSetting = true)
+        {
+            return config(group, name, value, new ConfigDescription(description), synchronizedSetting);
+        }
+
+        private class ConfigurationManagerAttributes
+        {
+            public bool? Browsable = false;
+        }
+
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,64 @@
+# Deeds of Utgard
+
+A super simple currency that was created for a server (now died out) by me. I am just uploading if someone else finds a
+use. It was used in their marketplace to buy plots of land.
+
+`The prefabs are not craftable, but you can buy them from the market (if you have one) or spawn them in.`
+
+`If the mod is installed on the server, it version checks with itself. Kicking players that don't have it.`
+
+## How they used the prefabs
+
+They chose to make "deeds" that would be sold by an NPC. These deeds all had a different cost and "size" for the plot of
+land they would buy. They used WardIsLove's custom radius to configure a protection for their plot of land depending on
+the size they bought. The admin would change the radius of the ward.
+
+I guess, if you come up with an implementation for your server, feel free to use
+this. `This mod is provided as is and will only recieve updates if requested. (This includes if an update to the game breaks the mod. I will only update if someone requests it.)`
+
+## Information
+
+#### Stack size = 50
+
+#### Weight = 0.1
+
+### Prefab Names
+
+- Normal Deeds <img src="https://i.imgur.com/OU0KYu2.png" align="center" width="64" height="64">
+    * Deed10m
+    * Deed20m
+    * Deed50m
+- Utgard Deeds (Red outline) <img src="https://i.imgur.com/robxZ5Y.png" align="center" width="64" height="64">
+    * DeedUtgard10m
+    * DeedUtgard20m
+    * DeedUtgard50m
+      <br>
+      <br>
+
+### Internal Token Names
+
+- ##### Normal Deeds
+    * $item_deed_10m
+    * $item_deed_20m
+    * $item_deed_50m
+- ##### Utgard Deeds (Red outline)
+    * $item_deed_utgard_10m
+    * $item_deed_utgard_20m
+    * $item_deed_utgard_50m
+
+# Author Information
+
+### Azumatt
+
+`DISCORD:` Azumatt#2625
+
+`STEAM:` https://steamcommunity.com/id/azumatt/
+
+For Questions or Comments, find me in the Odin Plus Team Discord or in mine:
+
+[![https://i.imgur.com/XXP6HCU.png](https://i.imgur.com/XXP6HCU.png)](https://discord.gg/Pb6bVMnFb2)
+<a href="https://discord.gg/pdHgy6Bsng"><img src="https://i.imgur.com/Xlcbmm9.png" href="https://discord.gg/pdHgy6Bsng" width="175" height="175"></a>
+***
+> # Update Information (Latest listed first)
+> ### v1.0.0
+> - Initial Release
